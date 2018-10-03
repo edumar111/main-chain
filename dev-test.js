2f61e2077f2af973d422902523ecacdf9e6c9ff2
Eduardo Marchena <emarchen@everis.com>
2018-10-03 17:03:18 -0500

    wallet and tarnsaction

--- dev-test.js
+++ dev-test.js
@@ -10,8 +10,13 @@
 
 //test de mineria 
 //npm run dev-test
-const Blockchain = require('./blockchain');
+
+/*const Blockchain = require('./blockchain');
 const bc = new Blockchain();
 for (let i = 0; i < 10; i++) {
     console.log(bc.addBlock(`foo ${i}`).toString());
-}
+}*/
+
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+console.log(wallet.toString())
