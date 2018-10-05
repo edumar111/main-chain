0e4194b6350411af0322c7469a8d52407c11dd58
Eduardo Marchena <emarchen@everis.com>
2018-10-04 21:12:04 -0500

    final-learning

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -51,6 +51,9 @@ class P2pServer {
                 case MESSAGE_TYPES.transaction:
                     this.transactionPool.updateOrAddTransaction(data.transaction);
                     break;
+                case MESSAGE_TYPES.clear_transactions:
+                    this.transactionPool.clear();
+                    break;
             }
         });
     }
@@ -68,6 +71,12 @@ class P2pServer {
     }
     broadcastTransaction(transaction) {
         this.sockets.forEach(socket => this.sendTransaction(socket, transaction));
+    }
+
+    broadcastClearTransactions() {
+        this.sockets.forEach(socket => socket.send(JSON.stringify({
+            type: MESSAGE_TYPES.clear_transactions
+        })));
     }
 
 }
